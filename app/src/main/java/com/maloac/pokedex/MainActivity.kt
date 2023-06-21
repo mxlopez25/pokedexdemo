4227be3e4a11cca52d83d92f55eedda6f31369ef
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2023-06-21 16:04:08 -0400

    PokedexDrawerMenu added as drawerContent

--- app/src/main/java/com/maloac/pokedex/MainActivity.kt
+++ app/src/main/java/com/maloac/pokedex/MainActivity.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.maloac.pokedex.components.HomeAppBar
+import com.maloac.pokedex.components.PokedexDraweMenu
 import com.maloac.pokedex.ui.theme.PokedexTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,8 +32,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PokedexApp() {
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
     Scaffold(
-        topBar = { HomeAppBar() }
+        scaffoldState = scaffoldState,
+        topBar = { HomeAppBar(scaffoldState, coroutineScope) },
+        drawerContent = { PokedexDraweMenu() }
     ) {
 
     }
