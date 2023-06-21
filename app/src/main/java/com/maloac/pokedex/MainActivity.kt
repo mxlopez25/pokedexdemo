938a569c579b52d3e12d704098ae8663ce9d1946
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2023-06-21 15:16:45 -0400

    Initial Commit

---
+++ app/src/main/java/com/maloac/pokedex/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.maloac.pokedex
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.maloac.pokedex.components.HomeAppBar
+import com.maloac.pokedex.ui.theme.PokedexTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PokedexTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    PokedexApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PokedexApp() {
+    Scaffold(
+        topBar = { HomeAppBar() }
+    ) {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PokedexTheme {
+        PokedexApp()
+    }
+}
