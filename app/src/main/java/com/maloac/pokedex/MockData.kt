007a60f86e991e41ea2a3f925cfb3ee812f9dfcf
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2023-06-22 14:29:47 -0400

    MockData added

---
+++ app/src/main/java/com/maloac/pokedex/MockData.kt
@@ -0,0 +1,38 @@
+package com.maloac.pokedex
+
+import com.maloac.pokedex.components.models.PokedexData
+
+val pokedexData = listOf(
+    PokedexData(
+        id = 1,
+        order = 1,
+        name = "Bulbasaur"
+    ),
+    PokedexData(
+        id = 2,
+        order = 2,
+        name = "Ivysaur"
+    ),
+    PokedexData(
+        id = 3,
+        order = 3,
+        name = "Venasaur"
+    ),
+    PokedexData(
+        id = 4,
+        order = 4,
+        name = "Charmander"
+    ),
+    PokedexData(
+        id = 5,
+        order = 5,
+        name = "Charmeleon"
+    ),
+    PokedexData(
+        id = 6,
+        order = 6,
+        name = "Blastoise"
+    ),
+
+
+)
