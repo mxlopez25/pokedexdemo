abdb85906c477db9689ded7bea1e2334e753d1a0
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2023-06-22 11:37:25 -0400

    Drawer made scrollable

--- app/src/main/java/com/maloac/pokedex/components/PokedexDrawerMenu.kt
+++ app/src/main/java/com/maloac/pokedex/components/PokedexDrawerMenu.kt
@@ -1,20 +1,21 @@
 package com.maloac.pokedex.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.maloac.pokedex.DrawerMenuData
+import com.maloac.pokedex.components.models.DrawerMenuData
 
 @Composable
-fun PokedexDrawerMenu() {
+fun PokedexDrawerMenu(scrollState: ScrollState) {
     val menuList = listOf(
         DrawerMenuData.Divider,
         DrawerMenuData.AllGenerationsHeader,
@@ -27,7 +28,7 @@ fun PokedexDrawerMenu() {
         DrawerMenuData.Divider,
         DrawerMenuData.ApiInformation
     )
-    Column {
+    Column(modifier = Modifier.verticalScroll(scrollState)) {
         Text(text = "Poke Menu",
             color = Color.Red,
             modifier = Modifier.padding(start = 20.dp, end = 20.dp),
