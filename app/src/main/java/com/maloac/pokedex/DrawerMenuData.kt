4bde5fa90a0a03003c537a74ed47cc4533b5d611
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2023-06-22 10:51:14 -0400

    DrawerMenuData added

---
+++ app/src/main/java/com/maloac/pokedex/DrawerMenuData.kt
@@ -0,0 +1,42 @@
+package com.maloac.pokedex
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Accessible
+import androidx.compose.material.icons.outlined.AllInbox
+import androidx.compose.material.icons.outlined.Android
+import androidx.compose.material.icons.outlined.Dry
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class DrawerMenuData (
+    val icon: ImageVector? = null, val title: String? = null,
+    val isDivider: Boolean = false, val isHeader: Boolean = false
+) {
+    object AllGenerations: DrawerMenuData(
+        icon = Icons.Outlined.AllInbox, title = "All Pokemons"
+    )
+    object FirstGeneration: DrawerMenuData(
+        icon = Icons.Outlined.Dry, title = "1st Gen"
+    )
+    object SecondGeneration: DrawerMenuData(
+        icon = Icons.Outlined.Android, title = "2nd Generation"
+    )
+
+
+    object ApiInformation: DrawerMenuData(
+        icon = Icons.Outlined.Accessible, title = "APIs Information"
+    )
+
+
+    object Divider: DrawerMenuData(
+        isDivider = true
+    )
+
+    object AllGenerationsHeader: DrawerMenuData(
+        isHeader = true,
+        title = "Generations"
+    )
+    object OtherInformations: DrawerMenuData(
+        isHeader = true,
+        title = "Miscs"
+    )
+}
